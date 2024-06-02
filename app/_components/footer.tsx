ace29f4d1ceac1601cacc61b30fd921c5b3b607d
valderez <valderezpe@gmail.com>
2024-06-02 13:59:55 -0300

    feat: add footer

---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-3 px-3">
+      <p className="text-gray-400 text-xs font-bold opacity-75">
+        © 2024 Copyright - solução mobile
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
