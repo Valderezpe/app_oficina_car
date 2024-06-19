af9d7ccb56415bfb9e059cce16eec8767de28edd
valderez <valderezpe@gmail.com>
2024-06-19 20:04:27 -0300

    feat: add components chevron

--- app/lojas/[id]/page.tsx
+++ app/lojas/[id]/page.tsx
@@ -1,9 +1,6 @@
 import { db } from "@/app/_lib/prisma";
 
-import { Button } from "@/app/_components/ui/button";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
-import Image from "next/image";
-import ServiceItem from "./_components/service-item";
+import LojasInfo from "./_components/lojas-infor";
 
 interface LojasDetailsPagePros {
   params: {
@@ -29,52 +26,54 @@ const LojasDetailsPage = async ({ params }: LojasDetailsPagePros) => {
     return null;
   }
   return (
-    <div>
-      <div className="h-[250px] w-full relative">
-        <Button
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-2 left-2"
-        >
-          <ChevronLeftIcon />
-        </Button>
+    <LojasInfo barbershop={barbershop} />
 
-        <Button
-          size="icon"
-          variant="outline"
-          className="z-50 absolute top-2 right-2"
-        >
-          <MenuIcon />
-        </Button>
-        <Image
-          src={barbershop.imageUrl}
-          fill
-          alt={barbershop.name}
-          style={{
-            objectFit: "cover",
-          }}
-          className="opacity-85"
-        />
+    // <div>
+    //   <div className="h-[250px] w-full relative">
+    //     <Button
+    //       size="icon"
+    //       variant="outline"
+    //       className="z-50 absolute top-2 left-2"
+    //     >
+    //       <ChevronLeftIcon />
+    //     </Button>
 
-        {/* <barbershop barbershop={barbershop} /> */}
+    //     <Button
+    //       size="icon"
+    //       variant="outline"
+    //       className="z-50 absolute top-2 right-2"
+    //     >
+    //       <MenuIcon />
+    //     </Button>
+    //     <Image
+    //       src={barbershop.imageUrl}
+    //       fill
+    //       alt={barbershop.name}
+    //       style={{
+    //         objectFit: "cover",
+    //       }}
+    //       className="opacity-85"
+    //     />
 
-        {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
-        ))}
-      </div>
-      <div className="px-5 py-3">
-        <h1 className="text-xl font-bold">{barbershop.name}</h1>
-        <div className="flex items-center gap-1 mt-2">
-          <MapPinIcon className="text-primary" size={18} />
-          <p className="text-sm">{barbershop.address}</p>
-        </div>
+    //     {/* <barbershop barbershop={barbershop} /> */}
 
-        <div className="flex items-center gap-1 mt-2">
-          <StarIcon className="text-primary" size={18} />
-          <p className="text-sm">5,0 (895 avaliações)</p>
-        </div>
-      </div>
-    </div>
+    //     {barbershop.services.map((service) => (
+    //       <ServiceItem key={service.id} service={service} />
+    //     ))}
+    //   </div>
+    //   <div className="px-5 py-3 pb-6 border-b border-solid border-secondary">
+    //     <h1 className="text-xl font-bold">{barbershop.name}</h1>
+    //     <div className="flex items-center gap-1 mt-2">
+    //       <MapPinIcon className="text-primary" size={18} />
+    //       <p className="text-sm">{barbershop.address}</p>
+    //     </div>
+
+    //     <div className="flex items-center gap-1 mt-2">
+    //       <StarIcon className="text-primary" size={18} />
+    //       <p className="text-sm">5,0 (895 avaliações)</p>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
