6103819fd348ef2b0c96754abe44e88d4f1ede57
valderez <valderezpe@gmail.com>
2024-06-19 18:54:14 -0300

    feat: add details reserva

--- app/lojas/[id]/page.tsx
+++ app/lojas/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { db } from "@/app/_lib/prisma";
-import LojasInfo from "./_components/lojas-infor";
+
+import { Button } from "@/app/_components/ui/button";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import ServiceItem from "./_components/service-item";
 
 interface LojasDetailsPagePros {
   params: {
@@ -12,16 +16,66 @@ const LojasDetailsPage = async ({ params }: LojasDetailsPagePros) => {
     return null;
   }
 
-  const lojasinfo = await db.barbershop.findUnique({
+  const barbershop = await db.barbershop.findUnique({
     where: {
       id: params.id,
     },
+    include: {
+      services: true,
+    },
   });
 
-  if (!lojasinfo) {
+  if (!barbershop) {
     return null;
   }
-  return <LojasInfo barbershop={lojasinfo} />;
+  return (
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button
+          size="icon"
+          variant="outline"
+          className="z-50 absolute top-2 left-2"
+        >
+          <ChevronLeftIcon />
+        </Button>
+
+        <Button
+          size="icon"
+          variant="outline"
+          className="z-50 absolute top-2 right-2"
+        >
+          <MenuIcon />
+        </Button>
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          alt={barbershop.name}
+          style={{
+            objectFit: "cover",
+          }}
+          className="opacity-85"
+        />
+
+        {/* <barbershop barbershop={barbershop} /> */}
+
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+      <div className="px-5 py-3">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (895 avaliações)</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LojasDetailsPage;
