42312d584062df969f5a1db52f1aebdda764327e
valderez <valderezpe@gmail.com>
2024-07-04 22:56:39 -0300

    feat: add migrate auth

--- app/lojas/[id]/page.tsx
+++ app/lojas/[id]/page.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/app/_lib/prisma";
 
 import LojasInfo from "./_components/lojas-infor";
+import ServiceItem from "./_components/service-item";
 
 interface LojasDetailsPagePros {
   params: {
@@ -26,54 +27,15 @@ const LojasDetailsPage = async ({ params }: LojasDetailsPagePros) => {
     return null;
   }
   return (
-    <LojasInfo barbershop={barbershop} />
+    <div>
+      <LojasInfo barbershop={barbershop} />
 
-    // <div>
-    //   <div className="h-[250px] w-full relative">
-    //     <Button
-    //       size="icon"
-    //       variant="outline"
-    //       className="z-50 absolute top-2 left-2"
-    //     >
-    //       <ChevronLeftIcon />
-    //     </Button>
-
-    //     <Button
-    //       size="icon"
-    //       variant="outline"
-    //       className="z-50 absolute top-2 right-2"
-    //     >
-    //       <MenuIcon />
-    //     </Button>
-    //     <Image
-    //       src={barbershop.imageUrl}
-    //       fill
-    //       alt={barbershop.name}
-    //       style={{
-    //         objectFit: "cover",
-    //       }}
-    //       className="opacity-85"
-    //     />
-
-    //     {/* <barbershop barbershop={barbershop} /> */}
-
-    //     {barbershop.services.map((service) => (
-    //       <ServiceItem key={service.id} service={service} />
-    //     ))}
-    //   </div>
-    //   <div className="px-5 py-3 pb-6 border-b border-solid border-secondary">
-    //     <h1 className="text-xl font-bold">{barbershop.name}</h1>
-    //     <div className="flex items-center gap-1 mt-2">
-    //       <MapPinIcon className="text-primary" size={18} />
-    //       <p className="text-sm">{barbershop.address}</p>
-    //     </div>
-
-    //     <div className="flex items-center gap-1 mt-2">
-    //       <StarIcon className="text-primary" size={18} />
-    //       <p className="text-sm">5,0 (895 avaliações)</p>
-    //     </div>
-    //   </div>
-    // </div>
+      <div className="px-5 flex flex-col gap-3 py-6">
+        {barbershop.services.map((service) => (
+          <ServiceItem key={service.id} service={service} />
+        ))}
+      </div>
+    </div>
   );
 };
 
