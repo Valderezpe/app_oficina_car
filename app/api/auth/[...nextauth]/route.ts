4570955c9699462394d3b2c210aa9758bd9a829b
valderez <valderezpe@gmail.com>
2024-07-05 16:54:10 -0300

    feat: add sign in logout

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,8 +8,8 @@ const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOLE_CLIENT_ID as string,
-      clientSecret: process.env.GPPGLE_CLIENT_SECRET as string,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
 });
